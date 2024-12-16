c4f2f53343cc0034581a3466622deb05b1458eea
Gabriel Silva <gabriel.gbss@gmail.com>
2024-12-16 18:57:40 -0300

    Deploy update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: '/portifolio',
+  assetPrefix: '/portifolio/',
   images: {
     unoptimized: true,
   },
 }
 
 module.exports = nextConfig
-
